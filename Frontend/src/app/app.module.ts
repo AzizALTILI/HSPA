37ea2f85ce4e3cad35cdb7e2f5e6d1f9f89820d8
AzizALTILI <aziz-altili@hotmail.com>
2020-12-01 22:26:24 +0300

    initial project

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,10 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PropertyCardComponent } from './property/property-card/property-card.component';
+import { NavBarComponent } from './property/nav-bar/nav-bar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertyCardComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
